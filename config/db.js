337fe14bdb5af7979398b56897083ca5836d1416
adriansito124 <adriangobara@gmail.com>
2025-06-13 14:35:39 -0300

    datatbadsse

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'aula',
+  host: 'bskfmtsmctpulje6jaet-mysql.services.clever-cloud.com',
+  user: 'ugb3v3m8mp3x28yr',
+  password: '1bMatujDD3J1qnRFg6Dv',
+  database: 'bskfmtsmctpulje6jaet',
 });
 
 connection.connect(err => {
